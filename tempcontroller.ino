c546bcc0baa4d77aa7f552375dd3293578eb7b80
Krillmeister <Krillmeister@gmail.com>
2014-08-19 22:13:48 +0200

    Skitsensorn läser tempen!
    koden ser dock ut som fan

--- tempcontroller.ino
+++ tempcontroller.ino
@@ -1,7 +1,19 @@
+
+/*
+
+
+Temp sensor datasheet:
+http://datasheets.maximintegrated.com/en/ds/DS18B20.pdf
+
+OneWire on pin 10
+*/
+
+
 #include <OneWire.h>
 
-OneWire onewire(10);
+OneWire onewire(10); // 4.7K pullup on pin
 
+int i;
 byte sensAddr[8];
 
 void setup() {
@@ -12,39 +24,64 @@ void setup() {
   onewire.select(sensAddr);
 
   // Upload sensor address to serial, ex 28 FF BA 6E 15 14 0 97
-  int i;
-  for ( i = 0 ; i<8; i++) {
-    Serial.print(sensAddr[i], HEX);
-    Serial.print(" ");
-  }
+  /*
+   for ( i = 0 ; i<8; i++) {
+   Serial.print(sensAddr[i], HEX);
+   Serial.print(" ");
+   }
+   */
 }
 
 
 void loop() {
-  int i;
   byte onewireIncData[8];
-  
+
   onewire.reset();
   onewire.select(sensAddr);
   onewire.write(0x44); // init temp reading
-  
-  delay(1000); //Temp reading takes a while to finish
+
+  delay(1000); // Temp reading takes a while to finish, should probably do something useful here
 
   onewire.reset();
   onewire.select(sensAddr);
   onewire.write(0xBE); //command to read memory
-   
-  Serial.println("Reading:");
-  
-  
+
+
   for (i=0; i<9; i++) {
     onewireIncData[i] = onewire.read();
-    Serial.print(onewireIncData[i], HEX);
-    Serial.print(" ");
+    //    Serial.print(onewireIncData[i], HEX);
+    //    Serial.print(" ");
   }  
 
-  while(1) {}
+  if (OneWire::crc8(onewireIncData,8) != onewireIncData[8]) {
+    Serial.println("CRC Mismatch, insert panic");
+  }
+
+  byte tempbyte[2];
+  tempbyte[0] = onewireIncData[1];  // Having the least significant bit first confuses and enrages me
+  tempbyte[1] = onewireIncData[0];
+
+  boolean isNegative;
+  isNegative = bitRead(tempbyte[0],7); // Bit 4-8 of the most significant byte are all sign bits indicating a negative number
+
+  tempbyte[0] = tempbyte[0] << 5;
+  tempbyte[0] = tempbyte[0] >> 5; // To get rid of the signing bits
+
+  unsigned int tempraw;
+  tempraw = tempbyte[1] | tempbyte[0] << 8;
+
+  float currtemp;
+  currtemp = tempraw * 0.0625;
+  
+  if (isNegative) { currtemp = currtemp *-1; }
+  
+  Serial.println(" ");  
+  Serial.print("Current temperature: ");
+  Serial.print(currtemp);
+  Serial.print("C");
+
 
 }
+
 
 
