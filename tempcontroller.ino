cd96af5dea80658f2f3c77fb5cc305be121faad6
Krillmeister <Krillmeister@gmail.com>
2014-08-20 21:21:18 +0200

    Revert "Made previous commit correctly this time..."
    This reverts commit c8592118d6971c445ecf96a064f5e7fec8dc3397.

--- tempcontroller.ino
+++ tempcontroller.ino
@@ -78,7 +78,7 @@ void loop() {
   Serial.println(" ");  
   Serial.print("Current temperature: ");
   Serial.print(currtemp);
-  Serial.print("\xf8C");
+  Serial.print(" C\xf8");
 
 
 
@@ -154,12 +154,12 @@ void draw(float currtemp, float targettemp) {
   u8g.setPrintPos(5, 10);
   u8g.print("Current temp: ");
   u8g.print(currtemp);
-  u8g.print("\xf8C");
+  u8g.print("C");
 
   u8g.setPrintPos(5, 20);  
   u8g.print("Target temp: ");
   u8g.print(targettemp);
-  u8g.print("\xf8C");
+  u8g.print("C");
 }
 
 
